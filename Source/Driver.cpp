d4b8b5206040c46dd7be6063fcd938beb99952d1
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2017-03-27 23:08:53 -0500

    Applied the Driver
    This is mainly incomplete as nothing truely exists yet, however this
    includes a base for us to work with.

---
+++ Source/Driver.cpp
@@ -0,0 +1,34 @@
+// ============================================================================================
+// --------------------------------------------------------------------------------------------
+// ============================================================================================
+// Team:			4
+// Programmer:		Jim, Nicholas, Ben, Kyle
+// Class:			CS 3003; S/T: Topics in Data Structures
+// Assignment #:	4
+// Due Date:		10.April.2017
+// Instructor:		Dr. Zhao
+// Description:		Simulation of Airline Reservation System
+//					<MORE DETAILS HERE>
+//
+// Notes:		Visual Basic 2015 [.NET] and MinGW
+// Exit Codes:
+//		0 = Successful operation
+//		!0 = Errors; check Operating System ExitCode definitions
+// ============================================================================================
+// --------------------------------------------------------------------------------------------
+// ============================================================================================
+
+
+
+
+// Main
+// ================
+// The main entry point for this program.
+int main()
+{
+	// OOP: Uncomment when these instructions are ready for invoktion.
+	// Reservation bluesky;
+	// bluesky.start();
+
+	return 0;
+} // main()
